8a43ee35ff9dccabdebae6819b751c5ccd3913a0
Lisa Walley <lwalley@eol.org>
2012-11-19 10:13:16 -0500

    Updated hps_browse, hps_events, hps_home, hps_news to use new hpszen panel layouts. Added creator as seperate column in index view, and removed more links for news and events if there are no news and events.

--- themes/custom/hpszen/layouts/hpszen_primary_box/hpszen-primary-box.tpl.php
+++ themes/custom/hpszen/layouts/hpszen_primary_box/hpszen-primary-box.tpl.php
@@ -1,15 +1,15 @@
 <?php
 /**
  * @file
- * Template for the HPS Zen responsive single column panel layout.
+ * Template for the HPS Zen responsive one box layout.
  *
  * Available variables:
  * - $content: An array of content, each item in the array is keyed to one
  *   panel of the layout. This layout supports the following sections:
- *   row-1-1: The only panel in the layout.
+ *   primary-box: The only panel in the layout.
  */
 ?>
-<div class="panel-hpszen-row-1-1">
-  <?php print $content['row-1-1']; ?>
+<div class="panel-hpszen-primary-box">
+  <?php print $content['primary-box']; ?>
 </div>
 
