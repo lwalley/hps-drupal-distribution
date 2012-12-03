80699cae8928ccc5d12bb74cf3f9f079d5295451
Lisa Walley <lwalley@eol.org>
2012-12-03 17:23:21 -0500

    Updates to panels and forms layouts. Added preprocess function to hide bistream URLs.

--- themes/custom/hpszen/template.php
+++ themes/custom/hpszen/template.php
@@ -256,10 +256,16 @@ function hpszen_preprocess_block(&$variables, $hook) {
  *
  * Override or insert variables into a field template.
  */
-/* -- Delete this line to use this function
 function hpszen_preprocess_field(&$variables, $hook) {
+  if ($variables['element']['#field_name'] == 'dspace_bitstream_url') {
+    // Hide bitstream URLs... @todo do this properly, maybe with custom views
+    // field?
+    foreach ($variables['items'] as $delta => $item) {
+      $variables['items'][$delta]['#access'] = FALSE;
+    }
+  }
 }
-//*/
+
 
 /**
  * Implements hook_preprocess_views_view().
