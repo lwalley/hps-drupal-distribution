984b6b7d4e42c283c55706e1ff5181307db7d57e
Lisa Walley <lwalley@eol.org>
2013-06-25 18:10:38 -0400

    Added new panels layouts i1, b2 and b3.

---
+++ themes/custom/hpszen/panels/layouts/hpszen_i1_b2/hpszen-i1-b2.tpl.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @file
+ * Template for the HPS Zen responsive introduction 1, basic 2 layout.
+ *
+ * Available variables:
+ * - $content: An array of content, each item in the array is keyed to one
+ *   panel of the layout. This layout supports the following sections:
+ *   - i1
+ *   - b1
+ *   - b2
+ */
+?>
+<div id="introduction">
+  <div class="section">
+    <div id="panel-hpszen-i1" class="panel">
+      <?php print $content['i1']; ?>
+    </div>
+  </div>
+</div>
+<div id="basic">
+  <div class="section">
+    <div id="panel-hpszen-b1" class="panel">
+      <?php print $content['b1']; ?>
+    </div>
+    <div id="panel-hpszen-b2" class="panel">
+      <?php print $content['b2']; ?>
+    </div>
+  </div>
+</div>
